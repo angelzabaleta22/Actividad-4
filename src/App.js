7f935c580b91338f0d54f844cb0dde300f13046d
Angel Zabaleta Torrez <angelzabaleta08@hotmail.com>
2023-03-24 09:46:49 -0500

    Se agregó el banner

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
 import { useState, useEffect } from "react";
-
 import "./App.css";
 import { TaskCreator } from "./components/TaskCreator";
+import { TaskBanner } from "./components/TaskBanner";
 import { TaskTable } from "./components/TaskTable";
 import { VisibilityControl } from "./components/VisibilityControl";
 import { Container } from "./components/Container";
 
 function App() {
+  const [userName, setUserName] = useState("Angel Zabaleta");
   const [tasksItems, setTasksItems] = useState([]);
   const [showCompleted, setShowCompleted] = useState(false);
+
+  useEffect(() => {
+    let data = localStorage.getItem("tasks");
+    if (data) {
+      setTasksItems(JSON.parse(data));
+    }
+    setUserName("Angel Zabaleta");
+  }, []);
 
   function createNewTask(taskName) {
     if (!tasksItems.find((task) => task.name === taskName)) {
@@ -43,6 +52,7 @@ function App() {
   }, [tasksItems]);
   return (
     <main className="bg-dark vh-100 text-white">
+      <TaskBanner userName={userName} taskItems={tasksItems} />
       <Container>
         <TaskCreator createNewTask={createNewTask} />
         <TaskTable tasks={tasksItems} toggleTask={toggleTask} />
